12cc05990d3d815b0ede4e1b7e0b6d53b1276b79
leevi-laitala <leevi.laitala@proton.me>
2024-01-29 16:12:15 +0200

    util: add slideshow launch script

---
+++ slideshow.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+SLIDESHOW_FOLDER="$HOME/Slideshow"
+SLIDESHOW_FILE="$HOME/slideshow-file"
+
+[[ ! -d "$SLIDESHOW_FOLDER" ]] && \
+	mkdir "$SLIDESHOW_FOLDER"
+
+function fatal() {
+	echo -e "FATAL: $1"
+	exit 1
+}
+
+function warning() {
+	echo -e "ERROR: $1"
+}
+
+function updateSlideshowFile() {
+	find "$SLIDESHOW_FOLDER" -type f | sort | sed -e 's/^/@/' -e 's/$/\n/' > "$SLIDESHOW_FILE"
+	[[ ! -s "$SLIDESHOW_FILE" ]] && \
+		fatal "No slides found in '$SLIDESHOW_FOLDER'"
+}
+
+function startSlideshow() {
+	timeout 1s xset q &> /dev/null || \
+		fatal "No X server running"
+
+	[[ ! -e "$SLIDESHOW_FILE" ]] && \
+		fatal "No slideshow file found, run first with 'update' command"
+
+	pidof sent > /dev/null && \
+		fatal "Slideshow already running"
+
+	sent "$SLIDESHOW_FILE" &
+}
+
+function killSlideshow() {
+	PID="$(pidof sent)"
+
+	[[ -z "$PID" ]] && \
+		fatal "No 'sent' instance running"
+
+	kill "$PID"
+	sleep "0.5s"
+
+	PID="$(pidof sent)"
+
+	[[ -n "$PID" ]] && \
+		fatal "Could not terminate 'sent' with pid $PID"
+}
+
+function printHelp() {
+	echo -e "usage: kiltisbulletin.sh COMMAND\n"
+	echo -e "Available commands:"
+	echo -e "  update\tUpdates slideshow config"
+	echo -e "  start\t\tStarts the slideshow"
+	echo -e "  kill\t\tTerminates running slideshow"
+}
+
+COMMAND="$(echo $1 | tr '[:upper:]' '[:lower:]')"
+
+case "$COMMAND" in
+    "update")
+        updateSlideshowFile
+        exit
+        ;;
+    "start")
+        startSlideshow
+        exit
+        ;;
+    "kill")
+        killSlideshow
+        exit
+        ;;
+    *)
+	printHelp
+	exit 1
+	;;
+esac
+
